37f0e68d1c14c94394cd3ba34e8f7c509972e586
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-11-09 11:53:23 -0600

    Updated docstring and added comments

--- class_composition_assignment/class_composition.py
+++ class_composition_assignment/class_composition.py
@@ -1,7 +1,15 @@
+""""
+Program: class_composition.py
+Author: Grayson Hardin
+Last date modified: 11/9/2020
+
+Program stores various info (such as first name, last name, etc) within a class called Pearson and then similar info that is applicable to a Student which is stored in a class called Student.
+"""
 import datetime
 
 
 class Person:
+    # List of variables for Pearson
     def __init__(self, person_id, last_name, first_name, phone_number, address):
         self.person_id = person_id
         self.last_name = last_name
@@ -11,20 +19,22 @@ class Person:
 
 
 class Student:
+    # List of variables for Student
     def __init__(self, person, gpa, major, start_date):
         self.person = person
         self.gpa = gpa
         self.major = major
         self.start_date = start_date
 
-    def change_major(self, value):
+    def change_major(self, value):  # Grants the ability to override Major
         self.major = value
 
-    def change_gpa(self, value):
+    def change_gpa(self, value):  # Grants the ability to override GPA
         self.gpa = value
 
     def display(self):
-        print(f'First name: {self.person.first_name}, \nLast Name: {self.person.last_name}, \nPhone number: {self.person.phone_number} \nAddress: {self.person.address} \nGPA is: {self.gpa} \nMajor is: {self.major}')
+        print(
+            f'First name: {self.person.first_name}, \nLast Name: {self.person.last_name}, \nPhone number: {self.person.phone_number} \nAddress: {self.person.address} \nGPA is: {self.gpa} \nMajor is: {self.major}')
 
 
 person = Person(1, 'Hardin', 'Grayson', '832-293-9912', '2020 Liberty St')
@@ -34,5 +44,4 @@ student = Student(person, 4.0, 'PolSci', datetime.date(2020, 1, 1))
 student.change_gpa(3.0)
 student.change_major('CIS')
 student.display()
-del(person, student)
-
+del (person, student)
