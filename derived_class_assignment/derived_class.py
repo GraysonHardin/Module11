2886aedf0e1b87aa489642d8003faf2c47e3f787
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-11-09 12:02:06 -0600

    Updated docstring and added comments

--- derived_class_assignment/derived_class.py
+++ derived_class_assignment/derived_class.py
@@ -1,6 +1,13 @@
-class Person:
-    """Person class"""
+""""
+Program: derived_class.py
+Author: Grayson Hardin
+Last date modified: 11/9/2020
 
+Program that prints student ID, name, and optional parameters.
+"""
+
+
+class Person:
     def __init__(self, lname, fname, addy=''):
         self._last_name = lname
         self._first_name = fname
@@ -17,18 +24,15 @@ class Student(Person):
         self._major = major
         self._gpa = gpa
 
-
     def display(self):
         return f'{self.student_id} {self._last_name} {self._first_name} {self._address} {self._major} {self._gpa}'
 
 
 # Driver
-my_student = Student(900111111, 'Hardin', 'Grayson')
+my_student = Student(900111111, 'Hardin', 'Grayson')  # Base requirements
 print(my_student.display())
-my_student = Student(900111111, 'Hardin', 'Grayson', 'Computer Engineering')
+my_student = Student(900111111, 'Hardin', 'Grayson', 'Computer Engineering')  # Added an optional value
 print(my_student.display())
-my_student = Student(900111111, 'Hardin', 'Grayson', 'Computer Engineering', 4.0)
+my_student = Student(900111111, 'Hardin', 'Grayson', 'Computer Engineering', 4.0)  # Added two optional values
 print(my_student.display())
 del my_student
-
-
